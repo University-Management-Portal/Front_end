fa7e0361459b1fe476f704506f67a33fb2c2693f
praveen kumar <praveenraja4493@gmail.com>
2026-01-08 22:28:11 +0530

    staff dashboard completed

--- ums/src/App.jsx
+++ ums/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css'
+import Header from './Components/Header/Header'
+import StaffSideBar from './Components/SideBar/StaffSideBar'
+import StaffDashboard from './Components/Staff/StaffDashboard'
+import Footer from './Components/Footer/Footer'
+
 function App() {
   return(
     <>
-
+    <Header />
+    <StaffDashboard />
     </>
   )
 }
