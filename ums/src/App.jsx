16afbdaa86e211382e5af9ef7d5ee3ff43dcc1b7
praveen kumar <praveenraja4493@gmail.com>
2026-01-09 09:01:58 +0530

    staff profile created

--- ums/src/App.jsx
+++ ums/src/App.jsx
@@ -4,11 +4,14 @@ import Header from './Components/Header/Header'
 import StaffSideBar from './Components/SideBar/StaffSideBar'
 import StaffDashboard from './Components/Staff/StaffDashboard'
 import Footer from './Components/Footer/Footer'
+import StaffProfile from './Components/Staff/StaffProfile'
 
 function App() {
   return(
     <>
-    
+    <Header />
+    <StaffProfile />
+    <Footer />
     </>
   )
 }
---
+++ ums/src/Components/Staff/StaffProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function StaffProfile() {
+  return (
+    <div className='cart'>
+
+        <img src="jdprofile1.jpg" alt="Profile" width={200} height={200} style={{borderRadius:"50%" , marginTop:"200px" , marginLeft:"200px"}}/>
+
+        <div className="update-photo" style={{width:"100px" , height:"60px" , marginLeft:"240px" , color:"#16005D"}}><button>Update Photo</button></div>
+
+        <button className="reset-button" style={{width:"160px" , height:"60px" , marginLeft:"240px" ,backgroundColor:"#16005D"}}>Reset Password</button>
+
+        <div className="logout-button"  style={{width:"100px" , height:"60px" , marginLeft:"240px" , color:"#16005D"}}><button>Logout</button></div>
+      
+    </div>
+  )
+} 
