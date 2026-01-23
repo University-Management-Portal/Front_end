b1de6ae5c283b7ce6e00aba88c4c8fc798ca275c
praveen kumar <praveenraja4493@gmail.com>
2026-01-23 11:28:53 +0530

    Container added

--- ums/src/Components/Staff/Courses.js
+++ ums/src/Components/Staff/Courses.js
@@ -1,9 +1,9 @@
 const courses = [
-    { sub: "CS104 / Advanced Networking", dept: "III Year CSE C" ,slug:"advanced-networking"},
-    { sub: "CS101 / Database Management System", dept: "III Year CSE C",slug:"database-management-system" },
-    { sub: "CS102 / Data Structure", dept: "III Year CSE C",slug:"data-structure" },
-    { sub: "CS104 / Advanced Networking", dept: "III Year CSE C",slug:"advanced-networking" },
-    { sub: "CS101 / Database Management System", dept: "III Year CSE C",slug:"database-management-system" },
-    { sub: "CS102 / Data Structure", dept: "III Year CSE C",slug:"data-structure" }
+    { sub: "CS104 / Advanced Networking", dept: "III Year CSE C" ,slug:"advanced-networking",image:"ad.jpg"},
+    { sub: "CS101 / Database Management System", dept: "III Year CSE C",slug:"database-management-system" ,image:"ad.jpg"},
+    { sub: "CS102 / Data Structure", dept: "III Year CSE C",slug:"data-structure" ,image:"ad.jpg"},
+    { sub: "CS104 / Advanced Networking", dept: "III Year CSE C",slug:"advanced-networking",image:"ad.jpg" },
+    { sub: "CS101 / Database Management System", dept: "III Year CSE C",slug:"database-management-system",image:"ad.jpg" },
+    { sub: "CS102 / Data Structure", dept: "III Year CSE C",slug:"data-structure" ,image:"ad.jpg"}
   ]
 export default courses;
--- ums/src/Components/Staff/Schedule.jsx
+++ ums/src/Components/Staff/Schedule.jsx
@@ -6,32 +6,19 @@ export default function Schedule() {
   return (
 
     <div className="div-schedule">
-      <div className="row-nav-page">
+
+        <a href="/timetable.pdf" target="_blank" rel="noopener noreferrer" className="btn view">
+        <div className="row-nav-page" >
         <p className="ti">Time Table</p>
-        <div className="btn-group">
-          <a href="/timetable.pdf" target="_blank" rel="noopener noreferrer" className="btn view">
-            <FaEye /> View
-          </a>
+        </div>  
+        </a>
 
-          <a href="/timetable.pdf" download className="btn download">
-            <FaDownload /> Download
-          </a>
-        </div>
-      </div>  
-
-
-      <div className="row-nav-page">
+      
+        <a href="/academic_calendar.pdf" target="_blank" rel="noopener noreferrer" className="btn view">
+        <div className="row-nav-page">
         <p className="ti">Academics calendar</p>
-        <div className="btn-group">
-          <a href="/academic_calendar.pdf" target="_blank" rel="noopener noreferrer" className="btn view">
-            <FaEye /> View
-          </a>
-
-          <a href="/academic_calendar.pdf" download className="btn download">
-            <FaDownload /> Download
-          </a>
         </div>
-      </div>
+        </a>
 
     </div>
 
